6c9f43dada8dec6c1cb4bcccf6d8aad4eee491f4
Diego Sandoval <cheval.diego@gmail.com>
2025-05-29 23:40:22 -0600

    Update Subnet output

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,7 +5,7 @@ output "vpc_id" {
 
 output "public_subnets_ids" {
   description = "List of IDs of public subnets"
-  value       = aws_subnet.public_subnet[*].id
+  value       = aws_subnet.public[*].id
 }
 
 output "vpc_endpoint_execute_api_id" {
